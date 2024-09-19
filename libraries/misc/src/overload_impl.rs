0543337887cd07d015879b1a839bab29ee7d8452
Lawrence Esswood <lesswood@google.com>
2024-09-19 22:46:49 +0000

    Add some misc helpers
    These don't really belong in kernel/utilities.
    A collection of helper types / macros
    
    *const_env
    Macro parse envionment variables as constant inits
    *default_array
    Trait to construct arrays of more than 32 elements
    *divorce
    Trait to describe a more dynamic borrowing semantic used DMA interfaces
    *never
    ! is not stable, and I wanted to use a consistent type
    *overload_impl
    A macro to do overloading of traits
    *potatoes
    Helpers to catch unintended destruction by panicking
    *take_borrow
    Helper to allow calling methods on things in Cell<>
    *tpanic
    Panic wrapper that behaves differently in tests
    *trait_alias
    Macro to create trait aliases.
    Also a workaround on our version of the compiler where bounds do not
    propagate properly.
    *unsigned_allocators
    Two allocators over unsigned integers
    
    Change-Id: Ib334c4bcdc075916477357073102b3f599994ff1

---
+++ libraries/misc/src/overload_impl.rs
@@ -0,0 +1,63 @@
+// Licensed under the Apache License, Version 2.0 or the MIT License.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+// Copyright Google LLC 2024.
+
+//! A helper type to allow statically overloading traits or implement foreign traits for foregin
+//! types.
+
+/// Create an overload for base, called overloaded.
+/// References to one can be treated as references to the other.
+/// You cannot rely on the overload having any constraints other than those imposed by
+/// the type it wraps.
+/// Usage:
+/// ```
+/// #![feature(const_mut_refs)]
+/// trait ForeignTrait{};
+/// struct ForeignType;
+/// // ...
+/// use misc::overload_impl;
+/// overload_impl!(MyWrapper);
+/// impl ForeignTrait for MyWrapper<ForeignType> {
+/// }
+/// ```
+/// The wrapped type is accessible via the public inner field.
+/// Transmute to the wrapper using `MyWrapper<ForeignType>::get(&unwrapped)`
+/// If you need to constrain a type with phantom data, there is second
+/// parameter to every type created for that purpose:
+/// e.g.: `MyWrapper<ForeignType, Key>.
+#[macro_export]
+macro_rules! overload_impl {
+    ($overloaded : ident) => {
+        #[repr(transparent)]
+        pub struct $overloaded<T, P = ()> {
+            pub inner: T,
+            _p: core::marker::PhantomData<P>,
+        }
+
+        // Safety: repr transparent around a struct with a single field will have exactly the same
+        // layout and alignment requirements as the wrapped type.
+        impl<T, P> $overloaded<T, P> {
+            #[inline]
+            pub const fn get(inner: &T) -> &Self {
+                unsafe { core::mem::transmute(inner) }
+            }
+            #[inline]
+            pub const fn get_mut(inner: &mut T) -> &mut Self {
+                unsafe { core::mem::transmute(inner) }
+            }
+        }
+
+        impl<T, P> core::ops::Deref for $overloaded<T, P> {
+            type Target = T;
+            fn deref(&self) -> &Self::Target {
+                &self.inner
+            }
+        }
+
+        impl<T, P> core::ops::DerefMut for $overloaded<T, P> {
+            fn deref_mut(&mut self) -> &mut Self::Target {
+                &mut self.inner
+            }
+        }
+    };
+}
